b7dfeb3a886564429ab116f95dfc40aae1e4020d
rajchicago <rajchicago1233@gmail.com>
2025-11-03 21:40:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
 
-search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chose, 'SEARCH_ON'].iloc[0]
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -16,6 +15,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col('SEARCH_ON'))
+search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chose, 'SEARCH_ON'].iloc[0]
 st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
