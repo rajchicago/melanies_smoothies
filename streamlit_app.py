1ece6781c469d993e4708c03a5997ac846fff8e2
rajchicago <rajchicago1233@gmail.com>
2025-11-03 20:56:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -32,7 +30,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -44,5 +42,5 @@ if ingredients_list:
         
         st.success(f"Your Smoothie is ordered,{name_on_order}!", icon="✅")
 # New section to display smoothiefroot nutrition information
-# st.text(smoothiefroot_response)
+ st.text(smoothiefroot_response)
 
